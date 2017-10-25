b124a5a3003ffbf541b8f5af5280813d869ee2dd
Ryosuke Kamesawa <emak.ryo@gmail.com>
2017-10-25 18:13:00 +0900

    add experiment

--- experiment/datasets.py
+++ experiment/datasets.py
@@ -24,3 +24,19 @@ def toy2(n_data=200, dim=50):
     x = s + np.random.randn(n_data, dim)
     y = np.sign(s @ alpha)
     return x, y, s
+
+def toy3(n_data=200, dim=50, n_active=3):
+    alpha = np.random.randn(n_active, 1)
+    active_dims = np.random.permutation(dim)[:n_active]
+    x = np.random.randn(n_data, dim)
+    s = x[:, active_dims]
+    y = np.sign(s @ alpha)
+    return x, y, s
+
+def toy4(n_data=200, dim=50, n_active=3):
+    alpha = np.random.randn(n_active, 1)
+    x = np.random.randn(n_data, dim)
+    active_dims = np.array([np.random.permutation(dim)[:n_active] for _ in range(n_data)])
+    s = np.array([xi[j] for xi, j in zip(x, active_dims)])
+    y = np.sign(s @ alpha)
+    return x, y, s
